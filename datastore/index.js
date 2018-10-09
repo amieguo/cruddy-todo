0004ffc98ca57b62d6d777920196a1789dfad432
amieguo <amieguo@gmail.com>
2018-10-08 20:16:11 -0700

    passed all tests

--- datastore/index.js
+++ datastore/index.js
@@ -78,22 +78,29 @@ exports.update = (id, text, callback) => {
   });
 };
 
-// if (!item) {
-//     callback(new Error(`No item with id: ${id}`));
-//   } else {
-//     items[id] = text;
-//     callback(null, { id, text });
-//   }
 
 exports.delete = (id, callback) => {
   var item = items[id];
-  delete items[id];
-  if (!item) {
-    // report an error if item not found
-    callback(new Error(`No item with id: ${id}`));
-  } else {
-    callback();
-  }
+  
+  fs.readFile(exports.dataDir + '/' + id + '.txt', 'utf8', (err, data) => {
+    if (err) {
+      callback(new Error(`No item with id: ${id}`));
+    } else {
+      fs.unlink(exports.dataDir + '/' + id + '.txt', (err)=> {
+        if (err) {
+          callback(new Error(`No item with id: ${id}`));
+        } else {
+          callback();
+        }
+      });
+    }
+  // delete items[id];
+  // if (!item) {
+  //   // report an error if item not found
+  //   callback(new Error(`No item with id: ${id}`));
+  // } else {
+  //   callback();
+  });
 };
 
 // Config+Initialization code -- DO NOT MODIFY /////////////////////////////////
